c69290dcbb4915ffa5c8b1075751a29a2f6be51a
alejandrosalesianos <alejandromartin2013@live.com>
2022-02-11 14:27:09 +0100

    primer commit

---
+++ MiarmaProject/src/main/java/com/salesianostriana/edu/MiarmaProject/MiarmaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.edu.MiarmaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiarmaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiarmaProjectApplication.class, args);
+	}
+
+}
---
+++ MiarmaProject/src/test/java/com/salesianostriana/edu/MiarmaProject/MiarmaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.edu.MiarmaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiarmaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
