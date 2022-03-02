f4f0a1b3e76014946a3b78e43c91753fd54fca6d
alejandrosalesianos <alejandromartin2013@live.com>
2022-03-02 14:22:02 +0100

    Cambios para el funcionamiento de miarmapp

--- MiarmaProject/src/main/java/com/salesianostriana/edu/MiarmaProject/users/controller/UserController.java
+++ MiarmaProject/src/main/java/com/salesianostriana/edu/MiarmaProject/users/controller/UserController.java
@@ -5,6 +5,7 @@ import com.salesianostriana.edu.MiarmaProject.users.dto.CreateUserDto;
 import com.salesianostriana.edu.MiarmaProject.users.dto.GetUserDto;
 import com.salesianostriana.edu.MiarmaProject.users.dto.UserDtoConverter;
 import com.salesianostriana.edu.MiarmaProject.users.model.UserEntity;
+import com.salesianostriana.edu.MiarmaProject.users.model.UserProfile;
 import com.salesianostriana.edu.MiarmaProject.users.services.UserEntityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,9 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.validation.Valid;
 import java.io.IOException;
 import java.net.URI;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,7 +29,20 @@ public class UserController {
     private final UserDtoConverter dtoConverter;
 
     @PostMapping("/")
-    public ResponseEntity<?> nuevoUser(@Valid @RequestPart("user") CreateUserDto userDto, @RequestPart("file")MultipartFile file) throws Exception, ListNotFoundException {
+    public ResponseEntity<?> nuevoUser(@RequestParam("telefono") String telefono, @Valid @RequestParam("nick") String nick, @RequestParam String fechaNacimiento, @RequestParam("perfil") UserProfile rol,  @RequestParam("password") String password,  @RequestParam("password2") String password2,  @RequestParam("email") String email, @RequestPart("file")MultipartFile file) throws Exception, ListNotFoundException {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        CreateUserDto userDto = CreateUserDto.builder()
+                .nick(nick)
+                .email(email)
+                .fechaNacimiento(LocalDate.parse(fechaNacimiento, formatter))
+                .telefono(telefono)
+                .perfil(rol)
+                .password(password)
+                .password2(password2)
+                .build();
+
         UserEntity user = userEntityService.saveUser(userDto,file);
         GetUserDto getUserDto = dtoConverter.UserEntityToGetUserDto(user);
         return ResponseEntity.created(URI.create(user.getFotoPerfil())).body(getUserDto);
